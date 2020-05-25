e41d91b13724b6bafbe7fd59a5f01b41fab4b3f9
Tim Oltman <toltman@gmail.com>
2020-05-25 14:05:51 -0400

    implemented course lists with navbars

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React from "react";
+import { Route, NavLink, Redirect } from "react-router-dom";
+import HTML from "./courses/HTML";
+import CSS from "./courses/CSS";
+import JavaScript from "./courses/JavaScript";
 
-const Courses = () => (
-  <div className="main-content courses">
-    <div className="course-header group">
-      <h2>Courses</h2> 
-      <ul className="course-nav">
-        <li><a href='/courses/html'>HTML</a></li>
-        <li><a href='/courses/css'>CSS</a></li>
-        <li><a href='/courses/javascript'>JavaScript</a></li>
-      </ul>
+const Courses = ({ match }) => {
+  return (
+    <div className="main-content courses">
+      <div className="course-header group">
+        <h2>Courses</h2>
+        <ul className="course-nav">
+          <li>
+            <NavLink to={`${match.url}/html`}>HTML</NavLink>
+          </li>
+          <li>
+            <NavLink to={`${match.url}/css`}>CSS</NavLink>
+          </li>
+          <li>
+            <NavLink to={`${match.url}/javascript`}>JavaScript</NavLink>
+          </li>
+        </ul>
+      </div>
+      <Route
+        exact
+        path={match.path}
+        render={() => <Redirect to={`${match.path}/html`} />}
+      />
+      <Route path={`${match.path}/html`} component={HTML} />
+      <Route path={`${match.path}/css`} component={CSS} />
+      <Route path={`${match.path}/javascript`} component={JavaScript} />
     </div>
-    
-    {/* Write routes here... */}
-  </div>
-);
+  );
+};
 
 export default Courses;
